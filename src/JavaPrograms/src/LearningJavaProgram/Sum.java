61a313c36570cf1ec5cb64a167fa1f37bb618189
Babita <Baviaryaan43@gmail.com>
2024-07-24 02:15:44 +0530

    Sum program is modified

--- src/JavaPrograms/src/LearningJavaProgram/Sum.java
+++ src/JavaPrograms/src/LearningJavaProgram/Sum.java
@@ -1,4 +1,20 @@
 package JavaPrograms.src.LearningJavaProgram;
 
+import java.util.Scanner;
+
 public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of a:");
+        int a = sc.nextInt();
+        System.out.println("Enter value of b:");
+        int b= sc.nextInt();
+
+        System.out.println("Sum of 2 nos is :" +(a+b));
+        System.out.println("Sub of 2 nos is :"+(a-b));
+        System.out.println("Multiplication of 2 nos is :"+(a*b));
+        System.out.println("Divide of 2 nos is :"+(a/b));
+        System.out.println("Division of 2 nos is :"+(a%b));
+
+    }
 }
